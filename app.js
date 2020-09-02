52578889f3450ea9d53b352396adfdb49e5c8219
Martin Spassov <martispassov@gmail.com>
2020-09-01 22:36:39 -0400

    game pieces created

--- app.js
+++ app.js
@@ -1,5 +1,54 @@
 document.addEventListener('DOMContentLoaded', function(){
-	alert('Hello there');
+	//Create the grid
+	var container = document.getElementById('grid');
+	for(var i=0; i<200; i++){
+		container.innerHTML += '<div></div>';
+	}
+
+	const grid = document.querySelector('#grid');
+	var squaresArray = Array.from(document.querySelectorAll('#grid div'));
+	var gameScore = document.querySelector('#score');
+	var startButton = document.querySelector('#startGame');
+	const offset = 10;
+
+	//Game pieces
+	const Lpiece = [
+		[1, offset+1, offset*2+1, 2],
+		[offset, offset+1, offset+2, offset*2+2],
+		[1, offset+1, offset*2+1, offset*2],
+		[offset, offset*2, offset*2+1, offset*2+2]
+	];
+
+	const Zpiece = [
+		[offset+1, offset+2, offset*2, offset*2+1],
+		[0, offset, offset+1, offset*2+1],
+		[offset+1, offset+2, offset*2, offset*2+1],
+		[0, offset, offset+1, offset*2+1]
+	]
+
+	const Tpiece = [
+		[1, offset, offset+1, offset+2],
+		[offset, offset+1, offset+2, offset*2+1],
+		[offset, 1, offset+1, offset*2+1],
+		[1, offset+1, offset*2+1, offset+2]
+	]
+
+	const Opiece = [
+		[0, 1, offset, offset+1],
+		[0, 1, offset, offset+1],
+		[0, 1, offset, offset+1],
+		[0, 1, offset, offset+1]
+	]
+
+	const iPiece = [
+		[1, offset+1, offset*2+1, offset*3+1],
+		[offset, offset+1, offset+2, offset+3],
+		[1, offset+1, offset*2+1, offset*3+1],
+		[offset, offset+1, offset+2, offset+3]
+	]
+
+
+
 
 
 
