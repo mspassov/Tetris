23523b71d9d70b22d5c8243bf8842a0d6a40412b
Martin Spassov <martispassov@gmail.com>
2020-09-06 22:39:25 -0400

    movement created

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 document.addEventListener('DOMContentLoaded', function(){
 	//Create the grid
 	var container = document.getElementById('grid');
-	for(var i=0; i<200; i++){
+	for(var i=0; i<190; i++){
 		container.innerHTML += '<div></div>';
+	}
+
+	for(var i=0; i<10; i++){
+		container.innerHTML += '<div class="taken"></div>';
 	}
 
 	const grid = document.querySelector('#grid');
@@ -13,16 +17,16 @@ document.addEventListener('DOMContentLoaded', function(){
 
 	//Game pieces
 	const Lpiece = [
-		[1, offset+1, offset*2+1, 2],
+		[1, offset+1, offset*2+1, 2],	//this represents each configuration of the piece
 		[offset, offset+1, offset+2, offset*2+2],
 		[1, offset+1, offset*2+1, offset*2],
 		[offset, offset*2, offset*2+1, offset*2+2]
 	];
 
 	const Zpiece = [
-		[offset+1, offset+2, offset*2, offset*2+1],
+		[1, 2, offset, offset+1],
 		[0, offset, offset+1, offset*2+1],
-		[offset+1, offset+2, offset*2, offset*2+1],
+		[1, 2, offset, offset+1],
 		[0, offset, offset+1, offset*2+1]
 	]
 
@@ -46,6 +50,53 @@ document.addEventListener('DOMContentLoaded', function(){
 		[1, offset+1, offset*2+1, offset*3+1],
 		[offset, offset+1, offset+2, offset+3]
 	]
+
+	var gamePieces = [Lpiece, Zpiece, Tpiece, Opiece, iPiece];
+	var startPos = 3;
+	var randomNum = Math.floor(Math.random()*gamePieces.length);
+	var currentRotation = 0;
+	var currentPiece = gamePieces[randomNum][currentRotation];	//grab a random piece
+
+	//create function to draw the different game pieces
+	function displayPiece(){
+		currentPiece.forEach(function(i){
+			squaresArray[startPos + i].classList.add('activePiece');
+		});
+
+	}
+
+	//create function to delete the game piece, so that it can move down
+	function deletePiece(){
+		currentPiece.forEach(function(i){
+			squaresArray[startPos + i].classList.remove('activePiece');
+		});
+	}
+	displayPiece();
+
+	function moveDownPiece(){
+		deletePiece();
+		startPos = startPos + offset;
+		displayPiece();
+		//freezePiece();
+	}
+
+	function freezePiece(){
+		if(currentPiece.some(function(i){squaresArray[startPos + i].classList.contains('taken')})){
+			currentPiece.forEach(function(i){
+				squaresArray[startPos + i].classList.add('taken');
+			});
+		}
+
+		//generate new game piece
+		randomNum = Math.floor(Math.random()*gamePieces.length);
+		currentPiece = gamePieces[randomNum][currentRotation];
+		startPos = 4;
+		displayPiece();
+	}
+
+	//timerID = setInterval(moveDownPiece, 100);
+
+
 
 
 
